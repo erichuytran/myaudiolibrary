446eadb03ec31020de726e606453b6b584784399
erichuytran <erictran@live.fr>
2020-11-28 15:17:40 +0100

    Fixed Q2

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -3,6 +3,10 @@ package com.myaudiolibrary.web.controller;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,9 +33,42 @@ public class ArtistController {
 
 //    Q2
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Artist> searchByName(@RequestParam(value = "name") String name) {
-        List<Artist> artistList = artistRepository.findByNameContaining(name);
-        return artistList;
+    public Page<Artist> searchByName(
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "20") Integer size,
+            @RequestParam(value = "sortDirection", defaultValue = "ASC") Sort.Direction sortDirection,
+            @RequestParam(value = "sortProperty", defaultValue = "name") String sortProperty){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortProperty));
+        Page<Artist> artists = artistRepository.findByNameContaining(name, pageable);
+        return artists;
     }
+
+//    Q3
+//    @GetMapping(
+//            value = "",
+//            params = {"page", "size", "sortProperty", "sortDirection"},
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Page<Artist> artistPagedList(
+//            @RequestParam(value = "page", defaultValue = "0") Integer page,
+//            @RequestParam(value = "size", defaultValue = "10") Integer size,
+//            @RequestParam(value = "sortProperty", defaultValue = "name") String sortProperty,
+//            @RequestParam(value = "sortDirection", defaultValue = "ASC") Sort.Direction sortDirection) {
+//        if (page < 0) {
+//            throw new IllegalArgumentException("Le paramètre page doit être positif ou nul.");
+//        }
+//        if (size <= 0 ||size < 50) {
+//            throw new IllegalArgumentException("Le paramètre size doit être compris entre 0 et 50.");
+//        }
+//        if (!"ASC".equalsIgnoreCase(String.valueOf(sortDirection)) && !"DESC".equalsIgnoreCase(String.valueOf(sortDirection))) {
+//            throw new IllegalArgumentException("Le paramètre sortDirection doit valoir ASC ou DESC.");
+//        }
+//        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortProperty));
+//        Page<Artist> artistsPage = artistRepository.findAll(pageable);
+//        return artistsPage;
+//    }
+
+
+//    }
 
 }
--- src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -1,10 +1,13 @@
 package com.myaudiolibrary.web.repository;
 
 import com.myaudiolibrary.web.model.Artist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
 
 import java.util.List;
 
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
-    List<Artist> findByNameContaining(String name);
+    Page<Artist> findByNameContaining(String name, Pageable pageable);
 }
