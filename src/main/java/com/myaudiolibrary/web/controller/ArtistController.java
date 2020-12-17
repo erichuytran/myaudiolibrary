b4fa01d57add626a92b1f3f0e3d85d3d0145322f
erichuytran <erictran@live.fr>
2020-12-18 00:06:50 +0100

    Q2 : Done (without pagination)

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -3,13 +3,20 @@ package com.myaudiolibrary.web.controller;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -19,6 +26,18 @@ public class ArtistController {
     @Autowired
     ArtistRepository artistRepository;
 
+    public void checkParams(Integer page, Integer size, Sort.Direction sortDirection) {
+        if (page < 0) {
+            throw new IllegalArgumentException("Le paramètre page doit être positif ou nul.");
+        }
+        if (size < 0 || size > 50) {
+            throw new IllegalArgumentException("Le paramètre size doit être compris entre 0 et 50.");
+        }
+        if (!"ASC".equalsIgnoreCase(String.valueOf(sortDirection)) && !"DESC".equalsIgnoreCase(String.valueOf(sortDirection))) {
+            throw new IllegalArgumentException("Le paramètre sortDirection doit valoir ASC ou DESC.");
+        }
+    }
+
     @GetMapping(value = "/{id}")
     public String getArtistById(@PathVariable Long id, final ModelMap model) {
         Optional<Artist> artistOptionnal = artistRepository.findById(id);
@@ -27,9 +46,19 @@ public class ArtistController {
         if (artistOptionnal.isEmpty()) {
             throw new EntityNotFoundException("L'artiste d'identifiant " + id + " n'a pas été trouvé");
         }
-
         model.put("artist", artistOptionnal.get());
+
         return "detailArtist";
+    }
+
+    @GetMapping(value = "", params = "name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String searchByName(final ModelMap model, @RequestParam(value = "name") String name){
+        List<Artist> artists = artistRepository.findByNameContainingIgnoreCase(name);
+        Integer totalArtists = artists.size();
+        model.put("artists", artists);
+        model.put("totalArtists", totalArtists);
+
+        return "listeArtists";
     }
 
 }
--- src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -3,5 +3,8 @@ package com.myaudiolibrary.web.repository;
 import com.myaudiolibrary.web.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
+    List<Artist> findByNameContainingIgnoreCase(String name);
 }
